b933d1b4b7346fc8427a4c63dd207e8caf219b94
MziM-dotcom <mabindisa947@gmail.com>
2025-06-04 08:19:16 +0200

    Add files via upload

---
+++ class.py
@@ -0,0 +1,10 @@
+class Person
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+personDetails = Person("Alice", 30)
+
+print(f"Name: {personDetails.name}, Age: {personDetails.age}")
+
